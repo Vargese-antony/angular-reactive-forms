4959340ca0cbf8fddb9b0aa747d982404f88858b
Vargese Antony <Vargese.antony@hotmail.com>
2018-02-12 12:24:33 -0600

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'pm-app',
+    template: `
+        <div class="container">
+          <pm-signup></pm-signup>
+        </div>
+    `
+})
+export class AppComponent { }
